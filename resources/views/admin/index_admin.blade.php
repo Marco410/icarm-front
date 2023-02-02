a597186a6439e2b62b30031719e10f9cabb6dcfe
Marco Castañeda <marco.castaneda@kiranalabs.mx>
2023-02-01 21:16:37 -0600

    Admin panel and routes

--- resources/views/admin/index_admin.blade.php
+++ resources/views/admin/index_admin.blade.php
@@ -1,28 +1,23 @@
-<?php $page="Dashboard";?>
+<?php $page = 'Dashboard'; ?>
 
 @extends('layout.mainlayout_admin')
-@section('content')		
-<div class="page-wrapper">
-			<div class="content container-fluid">
-			
-				<!-- Page Header -->
-				<div class="page-header">
-					<div class="row">
-						<div class="col-12">
-							
-						</div>
-					</div>
-				</div>
-				<!-- /Page Header -->
-				
-					
-		
-					
-					
-				</div>
-			</div>
-		</div> 
+@section('content')
+    <div class="page-wrapper">
+        <div class="content container-fluid">
+
+            <!-- Page Header -->
+            <div class="page-header">
+                <div class="row">
+                    <div class="col-12">
+
+                    </div>
+                </div>
+            </div>
+            <!-- /Page Header -->
+
         </div>
-		<!-- /Main Wrapper -->
-	   @endsection
-	  
+    </div>
+    </div>
+    </div>
+    <!-- /Main Wrapper -->
+@endsection
--- resources/views/layout/partials/nav_admin.blade.php
+++ resources/views/layout/partials/nav_admin.blade.php
@@ -2,33 +2,33 @@
 <div class="sidebar" id="sidebar">
     <div class="sidebar-logo">
         <a href="index">
-            <img src="{{ asset('assets_admin/img/user.jpg') }}" class="img-fluid" alt="">
+            <img src="{{ asset('assets/img/logo.png') }}" style="border-radius: 15px" class="img-fluid" alt="">
 
         </a>
     </div>
     <div class="sidebar-inner slimscroll">
         <div id="sidebar-menu" class="sidebar-menu">
             <ul>
-                    <li class="{{ Route::is(['dashboard']) ? 'active' : '' }}">
-                        <a href="{{ route('dashboard') }}"><i class="fas fa-columns"></i> <span>Inicio</span></a>
-                    </li>
+                <li class="{{ Route::is(['dashboard']) ? 'active' : '' }}">
+                    <a href="{{ route('dashboard') }}"><i class="fas fa-columns"></i> <span>Inicio</span></a>
+                </li>
 
-                    {{-- <li class="{{ Route::is(['encontrados']) ? 'active' : '' }}">
+                {{-- <li class="{{ Route::is(['encontrados']) ? 'active' : '' }}">
                         <a href="{{ route('encontrados') }}"><i class="fas fa-users"></i> <span>Encontrados</span></a>
                     </li> --}}
 
-                    <li class="{{ Route::is(['alumnos']) ? 'active' : '' }}">
-                        <a href="{{ route('alumnos') }}"><i class="fas fa-users"></i> <span>Alumnos</span></a>
-                    </li>
+                <li class="{{ Route::is(['eventos']) ? 'active' : '' }}">
+                    <a href="{{ route('eventos') }}"><i class="fas fa-calendar"></i> <span>Eventos</span></a>
+                </li>
 
-                    {{-- <li class="{{ Route::is(['filas']) ? 'active' : '' }}">
+                {{-- <li class="{{ Route::is(['filas']) ? 'active' : '' }}">
                         <a href="{{ route('filas') }}"><i class="fas fa-users"></i> <span>Filas</span></a>
                     </li>
 
                     <li class="{{ Route::is(['baja.view']) ? 'active' : '' }}">
                         <a href="{{ route('baja.view') }}"><i class="fas fa-users"></i> <span>Baja</span></a>
                     </li> --}}
-               
+
             </ul>
         </div>
     </div>
