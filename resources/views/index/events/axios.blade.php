a56b8f928202da341ce8c97fa88f92a379ef29b3
marco-castaneda <47765796+Marco410@users.noreply.github.com>
2023-07-09 13:45:13 -0600

    Information to event axios

--- resources/views/index/events/axios.blade.php
+++ resources/views/index/events/axios.blade.php
@@ -5,24 +5,148 @@
     <section class="hero">
         <img src="{{ asset('assets/img/index/events/axios.jpg') }}" class="back-video" alt="Axios">
 
-        <img style="border-radius: 15px; margin:20px; width: 100%" class="img-fluid serv-img" alt="Axios"
+        <img style="border-radius: 15px; margin:20px; width: 100%" class="img-fluid serv-img event-img" alt="Axios"
             src="{{ asset('assets/img/index/events/axios.jpg') }}">
+
     </section>
-    <section class="image-text-section">
+    <section class="container mt-4">
+        <div class="row ">
+            <div class="col-sm-12">
+                <div class="heading">
+                    <a href="#information">
+                        <h2>General Information <i class="fa fa-chevron-down"></i> </h2>
+                    </a>
+                    <span>Explora todos nuestros eventos</span>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mb-4">
         <div class="container">
             <div class="row">
-                <div class="col-sm-12 text-center">
-
-                </div>
                 <div class="col-sm-12 text-center">
                     <a href="{{ asset('assets/img/index/events/axios-2.jpg') }}">
                         <img style="border-radius: 15px; margin:10px; width: 100%" class="img-fluid serv-img" alt="Axios"
                             src="{{ asset('assets/img/index/events/axios-2.jpg') }}">
                     </a>
-
                 </div>
             </div>
 
+        </div>
+    </section>
+    <section id="information">
+        <div class="container">
+            <div class="row ">
+                <div class="col-sm-6">
+                    <div class="box">
+                        <h4>Registration</h4>
+                        <p>Please register online using the link at amoryrestauracion.tv or the QR code provided. Your
+                            registration will not be fully processed until you have paid.</p>
+                        <h4>Registro</h4>
+                        <p>Regístrese en línea utilizando el enlace en amoryrestauracion.tv o el código QR proporcionado. Su
+                            registro no se procesa por completo hasta que se haya pagado.</p>
+                    </div>
+                </div>
+                <div class="col-sm-6 ">
+                    <div class="box">
+                        <h4>Attire:</h4>
+                        <p>
+                            Dress comfortably! There will be indoor and outdoor activities. We recommend you throw your
+                            deodorant,
+                            body spray and extra clothes and towel in your bag in case your clothes get soiled or wet.
+                            Avoid wearing clothes that are see through, too short, low-cut, too tight or with inappropriate
+                            images
+                            or text.</p>
+                        <h4>Atuendo:</h4>
+                        <p>¡Vístete cómodamente! Habrá actividades en el interior y al aire libre. Te recomendamos que te
+                            traigas
+                            tu desodorante, spray para el cuerpo, extra ropa y toalla en tu bolso en caso de que tu ropa se
+                            ensucie
+                            o moje. Evita usar ropa transparente, demasiado corta, escotada, demasiado ajustada o con
+                            imágenes o
+                            texto inapropiados.</p>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="box">
+                        <h4>Meals:</h4>
+                        <p>Jueves - Dinner
+                            Viernes - Breakfast Snack, Lunch and Dinner
+                            Sabado - Breakfast Snack and Lunch</p>
+                        <h4>Comidas:</h4>
+                        <p>Jueves - Cena
+                            Viernes - Desayuno Merienda, Almuerzo y Cena
+                            Sábado - Desayuno Merienda y Almuerzo</p>
+
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="box">
+                        <h4>Merch/Snacks for sale: </h4>
+                        <p>Meals will be provided but if you would like to buy snacks or merch throughout the day bring
+                            cash.</p>
+                        <h4>Venta de merchandising/snacks:</h4>
+                        <p>Se proporcionarán comidas, pero si deseas comprar refrigerios o mercadería durante el día, trae
+                            efectivo.</p>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="box text-center">
+                        <h4>Parent conference date and time/Fecha y hora de la conferencia de padres</h4>
+                        <p>There will be 3 workshops for parents on Friday from 5pm to 9pm with small coffee breaks in
+                            between.</p>
+
+                        <p>Habrán 3 talleres para padres el viernes de 5pm a 9pm con pequeños recesos con café entre
+                            talleres.</p>
+
+                        <h4>Parent drop off and pick up locations/Ubicaciones para dejar y recoger a los adolescentes</h4>
+
+                        <p>You will drop off and pick up your kids at the main entrance.</p>
+
+                        <p>Podrán dejar y recoger a sus hijos en la entrada principal de la iglesia.</p>
+
+                    </div>
+
+                </div>
+                <div class="col-sm-12">
+                    <div class="box text-center">
+                        <h4>What will the day look like? / ¿Cómo es el programa?</h4>
+
+                        <h4>Jueves</h4>
+                        <p><strong>4:00 PM</strong> Registration & team assignment./ Revisión de inscripciones y asignación
+                            de equipos <br>
+                            <strong>5:00 PM</strong> Praise and worship. /Alabanza<br>
+                            <strong>6:00 PM</strong> Conference #1. /Conferencia #1<br>
+                            <strong>7:30 PM</strong> Dinner and meeting time./ Cena y tiempo de encuentro<br>
+                            <strong>8:30 PM</strong> Parents pick up/ Salida
+                        </p>
+
+                        <h5>Viernes </h5>
+                        <p><strong>9:00 AM</strong> Praise and worship and devotional/ Alabanza y devocional<br>
+                            <strong>10:30 AM</strong> Workshops/ Talleres<br>
+                            <strong>11: 30 AM</strong> Snack/ Merienda<br>
+                            <strong>11:45 AM</strong> Workshops/ Talleres<br>
+                            <strong>12:45 PM</strong> Lunch/ Comida<br>
+                            <strong>1:45 PM</strong> Free time (elective activity)/ Tiempo Libre (actividad electiva)<br>
+                            <strong>3:00 PM</strong> Outside games (Rally)/ Juegos al aire libre (Rally)<br>
+                            <strong>5:00 PM</strong> Time to freshen up/ Tiempo para refrescarse<br>
+                            <strong>5:30 PM</strong> Dinner/ Cena<br>
+                            <strong>6:00 PM</strong> Praise and Worship/ Alabanza<br>
+                            <strong>6:30 PM</strong> Conference #2/ Conferencia #2<br>
+                            <strong>8:00 PM</strong> Parents pick up/ Salida
+                        </p>
+
+                        <h5>Sábado </h5>
+                        <p><strong> 9:00 AM</strong> Praise and Worship/ Alabanza<br>
+                            <strong>10:00 AM</strong> Conference #3/ Conferencia #3<br>
+                            <strong>12:00 PM</strong> Lunch/ Comida<br>
+                            <strong>1:00 PM</strong> Outside games/ Juegos al aire libre<br>
+                            <strong>4:00 PM</strong> Parent’s pick up/ Salida
+                        </p>
+
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 
